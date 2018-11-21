0503676310711b671f569c5bbb05cc4966a533e2
reddowell <vikt.anmail@gmail.com>
2018-11-21 15:56:53 +0300

    tic tac toe game

--- funBox.ino
+++ funBox.ino
@@ -38,7 +38,7 @@ void loop()
       c = checkChosenGame(x, y);
       switch (c) {
         case 1:
-          Tft.lcd_clear_screen(RED);
+          ticTacToeGame();
           break;
         case 2:
           Tft.lcd_clear_screen(BLACK);
@@ -70,6 +70,149 @@ int checkChosenGame(int x, int y)
     return 2;
   }
   return 0;
+}
+
+void drawTicTacToeField()
+{
+  Tft.lcd_fill_rect(70, 40, 15, 240, WHITE);
+  Tft.lcd_fill_rect(150, 40, 15, 240, WHITE);
+  Tft.lcd_fill_rect(15, 95, 190, 15, WHITE);
+  Tft.lcd_fill_rect(15, 210, 190, 15, WHITE);
+///
+}
+
+void checkTicTacToeGame()
+{
+  int board[3][9] = {
+    {5, 6 ,7},
+    {8, 9, 10},
+    {13, 12 ,11}
+  };
+    
+  uint16_t xp = 0;
+  uint16_t yp = 0;
+  int posX = 0;
+  int posY = 0;
+  int sleep = 1;
+  int player = 1;
+  while(true){
+    sleep = Xpt.xpt2046_twice_read_xy(&xp, &yp);
+    if(!sleep){
+      posX = checkPosX(xp);
+      posY = checkPosY(yp);
+      if((posX != 0) && (posY != 0)){
+        board[posY - 1][posX - 1] = player;
+        drawFigure(player, posX, posY);
+        if(checkWin(board, player)){
+          Tft.lcd_clear_screen(WHITE);
+          if(player == 1){
+                      Tft.lcd_display_string(60, 120, (const uint8_t *)"CROSS WON", FONT_1608, RED);
+            } else {
+                      Tft.lcd_display_string(60, 120, (const uint8_t *)"ZERO WON", FONT_1608, RED);
+              }
+          delay(2000);
+          return 0;
+         }
+        if(player == 1){
+        player = 2;  
+        } else {
+        player = 1;  
+        }
+       }
+    }
+  }  
+}
+
+int checkWin(int board[3][9], int player)
+{
+  if(board[0][0] ==  board[0][1] && board[0][1] == board[0][2]){
+    return 1;
+  }
+                        Serial.println(board[1][0]);
+                        Serial.println(board[1][1]);
+                        Serial.println(board[1][2]);
+    if(board[1][0] ==  board[1][1] && board[1][1] == board[1][2]){
+      Serial.println("df");
+    return 1;
+  }
+    if(board[2][0] ==  board[2][1] && board[2][1] == board[2][2]){
+    return 1;
+  }
+      if(board[0][0] ==  board[1][0] && board[1][0] == board[2][0]){
+    return 1;
+  }
+        if(board[0][1] ==  board[1][1] && board[1][1] == board[2][1]){
+    return 1;
+  }
+        if(board[0][2] ==  board[1][2] && board[1][2] == board[2][2]){
+    return 1;
+  }
+
+          if(board[0][0] ==  board[1][1] && board[1][1] == board[2][2]){
+    return 1;
+  }
+            if(board[0][2] ==  board[1][1] && board[1][1]== board[2][1]){
+    return 1;
+  }
+  return 0;
+}
+
+void drawFigure(int player, int x, int y)
+{
+  x = x - 1;
+  int fy = y;
+  y = y * 60;
+  if(fy == 2){
+    y += 40;
+    }
+      if(fy == 3){
+    y += 72;
+    }
+    if(player == 2) {
+      Tft.lcd_draw_circle(x * 80 + 40, y, 25, RED);
+    } else {
+       Tft.lcd_draw_line(x * 80 + 25, y, x * 80 + 40, y + 25 , BLUE);
+       Tft.lcd_draw_line(x * 80 + 40, y, x * 80 + 25, y + 25, BLUE);
+    }
+}
+
+int checkPosX(uint16_t x)
+{
+  if(x <= 1850 && x >= 1450){
+    return 1;
+  }
+
+  if(x <= 1250 && x >= 800){
+    return 2;
+  }
+  
+  if(x <= 650 && x >= 200){
+    return 3;
+  }
+  return 0;
+}
+
+int checkPosY(uint16_t y)
+{
+   if(y <= 1850 && y >= 1450){
+    return 1;
+  }
+
+  if(y <= 1250 && y >= 800){
+    return 2;
+  }
+  
+  if(y <= 650 && y >= 200){
+    return 3;
+  }
+  return 0;
+}
+
+void ticTacToeGame()
+{
+  Tft.lcd_clear_screen(BLACK);
+  drawTicTacToeField();
+  checkTicTacToeGame(); 
 }
 
 void paint()
